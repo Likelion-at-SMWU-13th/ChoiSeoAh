e526a5452e8fefcf41ba13499d6b88ed509a8323
seoah <24seoah@sookmyung.ac.kr>
2025-04-09 01:13:27 +0900

    모델작성

--- django/week01/user/models.py
+++ django/week01/user/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    profile = models.ImageField(verbose_name='프로필')
+    real_name = models.CharField(max_length=20, verbose_name='실명')
+    user_name = models.CharField(max_length=20, verbose_name='아이디')
+    bio = models.TextField(blank=True , verbose_name='자기소개')
+
+
