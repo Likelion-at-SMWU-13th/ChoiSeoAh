dc55ad303a41301a66eb333151ee70e263e8e3bd
seoah <24seoah@sookmyung.ac.kr>
2025-07-15 19:22:00 +0900

    Feat : @RestController로 Post CRUD 구현

---
+++ spring/week01/seminar/src/main/java/com/likelion/seminar/post/controller/PostController.java
@@ -0,0 +1,63 @@
+package com.likelion.seminar.post.controller;
+
+import com.likelion.seminar.post.dto.PostDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("post")
+public class PostController {
+    private final List<PostDTO> postDTOList;
+
+    @PostMapping()
+    public void createPost(@RequestBody PostDTO postDTO) {
+        System.out.println(postDTO.toString());
+        this.postDTOList.add(postDTO);
+    }
+
+    @GetMapping()
+    public List<PostDTO> readPostAll() {
+        System.out.println("포스트 전체 조회");
+        return this.postDTOList;
+    }
+
+
+    @GetMapping("{id}")
+    public PostDTO readPost(@PathVariable("id") int id) {
+        System.out.println("포스트 단일 조회");
+        return this.postDTOList.get(id);
+    }
+
+    @GetMapping("param")
+    public PostDTO readPost_param(@RequestParam("id") int id) {
+        System.out.println("포스트 단일 조회2");
+        return this.postDTOList.get(id);
+    }
+
+    @PutMapping("{id}")
+    public void updatePost(
+            @PathVariable("id") int id,
+            @RequestBody PostDTO postDto
+    ) {
+        PostDTO targetPost = this.postDTOList.get(id);
+        if(postDto.getTitle()!=null){
+            targetPost.setTitle(postDto.getTitle());
+        }
+        if(postDto.getContent()!=null){
+            targetPost.setContent(postDto.getContent());
+        }
+        if(postDto.getWriter()!=null){
+            targetPost.setWriter(postDto.getWriter());
+        }
+        this.postDTOList.set(id, targetPost);
+    }
+
+    @DeleteMapping("{id}")
+    public void deletePost(@PathVariable("id") int id) {
+        this.postDTOList.remove(id);
+    }
+
+}
