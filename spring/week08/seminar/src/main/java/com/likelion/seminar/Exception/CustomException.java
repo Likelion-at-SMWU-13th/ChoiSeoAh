1f2b56ab9f38926fcc5700c915b5fced8589ddd9
seoah <24seoah@sookmyung.ac.kr>
2025-09-16 19:36:32 +0900

    Feat : CustomException 구현

---
+++ spring/week08/seminar/src/main/java/com/likelion/seminar/Exception/CustomException.java
@@ -0,0 +1,30 @@
+package com.likelion.seminar.Exception;
+
+
+
+public class CustomException extends RuntimeException {
+
+    private final HttpStatus httpStatus;
+
+    public CustomException(HttpStatus httpStatus, String message) {
+        super(message);
+        this.httpStatus = httpStatus;
+    }
+
+    public CustomException(HttpStatus httpStatus, String message, Throwable cause) {
+        super(message, cause);
+        this.httpStatus = httpStatus;
+    }
+
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+
+    @Override
+    public String toString() {
+        return "CustomException{" +
+                "httpStatus=" + httpStatus.getCode() + " " + httpStatus.getMessage() +
+                ", message='" + getMessage() + '\'' +
+                '}';
+    }
+}
