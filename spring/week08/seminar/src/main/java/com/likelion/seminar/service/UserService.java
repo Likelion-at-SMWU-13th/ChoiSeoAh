1953e688c9cc326d228061c7fdeec698a4cd447f
seoah <24seoah@sookmyung.ac.kr>
2025-09-21 20:32:28 +0900

    Feat : UserController 생성

--- spring/week08/seminar/src/main/java/com/likelion/seminar/service/UserService.java
+++ spring/week08/seminar/src/main/java/com/likelion/seminar/service/UserService.java
@@ -3,6 +3,7 @@ package com.likelion.seminar.service;
 import com.likelion.seminar.Exception.CustomException;
 import com.likelion.seminar.Exception.HttpStatus;
 import com.likelion.seminar.Repository.UserRepository;
+import com.likelion.seminar.dto.SignupRequestDTO;
 import com.likelion.seminar.entity.User;
 import org.springframework.stereotype.Service;
 
@@ -15,18 +16,23 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-    public void signup(User user) {
+    public void signup(SignupRequestDTO request) {
 
         // 아이디 중복 체크
-        if (userRepository.existsByUsername(user.getUsername()) {
+        if (userRepository.existsByUsername(request.getUsername()) {
             throw new CustomException(HttpStatus.CONFLICT, "이미 존재하는 아이디입니다.");
         }
         // 이메일 형식 검증
-        String email = user.getEmail();
+        String email = request.getEmail();
         if (email == null || !email.endsWith("@sookmyung.ac.kr")) {
             throw new CustomException(HttpStatus.BAD_REQUEST,"이메일은 @sookmyung.ac.kr로 끝나야 합니다.");
         }
 
+       User user = User.builder()
+               .username(request.getUsername())
+               .email(email)
+               .password(request.getPassword())
+               .bulid();
         userRepository.save(user);
 
     }
