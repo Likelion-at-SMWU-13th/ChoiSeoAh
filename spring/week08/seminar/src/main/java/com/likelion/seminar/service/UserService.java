7bf2172aa2c5d267cef2f12c5182ec4e138cd0fd
seoah <24seoah@sookmyung.ac.kr>
2025-09-21 20:37:12 +0900

    Feat : HttpStatus 에 CONFLICT 추가

--- spring/week08/seminar/src/main/java/com/likelion/seminar/service/UserService.java
+++ spring/week08/seminar/src/main/java/com/likelion/seminar/service/UserService.java
@@ -6,6 +6,8 @@ import com.likelion.seminar.Repository.UserRepository;
 import com.likelion.seminar.dto.SignupRequestDTO;
 import com.likelion.seminar.entity.User;
 import org.springframework.stereotype.Service;
+import static com.likelion.seminar.Exception.HttpStatus.CONFLICT;
+import static com.likelion.seminar.Exception.HttpStatus.BAD_REQUEST;
 
 @Service
 public class UserService {
@@ -20,12 +22,12 @@ public class UserService {
 
         // 아이디 중복 체크
         if (userRepository.existsByUsername(request.getUsername()) {
-            throw new CustomException(HttpStatus.CONFLICT, "이미 존재하는 아이디입니다.");
+            throw new CustomException(CONFLICT, "이미 존재하는 아이디입니다.");
         }
         // 이메일 형식 검증
         String email = request.getEmail();
         if (email == null || !email.endsWith("@sookmyung.ac.kr")) {
-            throw new CustomException(HttpStatus.BAD_REQUEST,"이메일은 @sookmyung.ac.kr로 끝나야 합니다.");
+            throw new CustomException(BAD_REQUEST,"이메일은 @sookmyung.ac.kr로 끝나야 합니다.");
         }
 
        User user = User.builder()
